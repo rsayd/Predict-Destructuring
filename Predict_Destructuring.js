22759f694341387865d59ec46e6110d3eab65f96
Ryan <rs77fl@gmail.com>
2023-09-27 14:10:15 -0700

    first commit

---
+++ Predict_Destructuring.js
@@ -0,0 +1,104 @@
+
+//Predict Destructuring
+//For the following five blocks of code, predict the output. If a line of code will throw an error, state the error.
+
+
+// Problem #1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [randomCar] = cars
+const [, otherRandomCar] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//Answer: Tesla
+//Mercedes
+
+
+
+
+
+// Problem #2
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//Answer: Name will not log it is noy defined
+//console.log(otherName) logs Elon
+
+
+
+
+
+// Problem  #3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+
+//Answer: console.log(password) logs 12345
+//console.log(hashedPassword) is undefined
+
+
+
+
+
+// Problem #4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [, first] = numbers;
+const [, , , second] = numbers;
+const [, , , , , , , , third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//Answer: console.log(first == second) logs false
+// console.log(first == third) logs true 
+
+
+
+
+
+// Problem #5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [, willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+
+//Answer:
+// console.log(key) logs value
+//console.log(secondKey) logs [1,5,1,8,3,3]
+//console.log(secondKey[0]) logs 1
+//console.log(willThisWork) logs 5
+
+
+
+
+
